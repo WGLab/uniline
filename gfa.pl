0f087a75e301636299c93dd2c155b51a6c96f4c9
Yunfei Guo <guoyunfei1989@gmail.com>
2015-08-18 12:08:45 -0700

    backup

--- gfa.pl
+++ gfa.pl
@@ -5,9 +5,11 @@ use lib "/home/yunfeiguo/projects/SeqMule/lib";
 use SeqMule::Utils;
 use Data::Dumper;
 
-my $FLANKSIZE = 100;
-my $nproc = 12;
-my $filter = {qcov=>95,idt=>95,e=>1};
+my $FLANKSIZE = 100; #length of flank sequences for anchoring
+my $MIN_GAP_DIST = 2*$FLANKSIZE; #min distance allowed between two gaps
+my $GAPLEN_MIS_TOLERANCE = 100; #max difference between filled gap and original gap
+my $nproc = 12; #nproc for blast
+my $filter = {qcov=>95,idt=>95,e=>1e-8}; #mapping filters
 my $debug = 1;
 
 
@@ -21,7 +23,9 @@ my $total = 0;
 my $unfilled = 0;
 
 if(0) {
+    my $bed_for_merge = "/tmp/".rand($$).".tmp.bed.formerge";
     !system("./extract_gaps.pl $fa > $gap") or die "Gap profiling failed\n" or die "$!\n";
+    !system("cat $gap > $bed_for_merge") and !system("bedtools merge -i $bed_for_merge -d $MIN_GAP_DIST > $gap") or die "failed to merge: $!\n";
     !system("/home/yunfeiguo/scripts/seq_related/fa2size.pl $fa > $genome") or die "Genome file creation failed\n";
     #prepare blast index
     !system("makeblastdb -in $db -dbtype nucl") or die "$!\n";
@@ -29,6 +33,7 @@ if(0) {
 }
 if(1) {
     for my $onegapRecord(&readBED($gap)) {
+	#next unless $onegapRecord->[0] =~ /chr17_KI270729v1_random/;
 	my $onegap = &SeqMule::Utils::genBED([$onegapRecord]);
 	my $gapid = join("_",@$onegapRecord);
 	my $oneflank = "$onegap.flank.bed";
@@ -38,7 +43,7 @@ if(1) {
 	!system("bedtools getfasta -fi $fa -bed $oneflank -fo $oneflankfa") or die "BED2FA failed: $!\n";
 	warn "$oneflank" if $debug;
 	warn "$oneflankfa" if $debug;
-	!system("blastn -db $db -query $oneflankfa -num_threads $nproc -task megablast -max_target_seqs 1 -outfmt '10 qseqid qlen sseqid qstart qend sstart send sstrand evalue pident qcovs' > $oneresult") or die "blast failed: $!\n";
+	!system("blastn -db $db -query $oneflankfa -num_threads $nproc -task megablast -max_target_seqs 1 -outfmt '10 qseqid qlen sseqid slen qstart qend sstart send sstrand evalue pident qcovs' > $oneresult") or die "blast failed: $!\n";
 	warn "mapping for $gapid: $oneresult\n" if $debug;
 	my @filledGap = &fillGap({source=>$fa,id=>$gapid,filter=>$filter,mapping=>$oneresult,db=>$db,gapRecord=>$onegapRecord});
 	if(@filledGap) {
@@ -89,24 +94,41 @@ sub fillGap {
     $mapping = &filterMapping($filter,$mapping);
     if(&ifTwinMapping($mapping)) {
 	warn  "twin mapping\n" if $debug;
-	if(1) {
-
-	} else {
-
-	}
+	    return(&twoAnchoredGap({gapRecord=>$gapRecord,mapping=>$mapping,fai=>$fai,id=>$id}));
     } else {
 	warn  "not twin mapping\n" if $debug;
 	#if gap occurs at an edge of the target assembly contig
 	#then only one sequence can be mapped
 	#when the gap doesn't occur the at edge, it is still possible to have only one sequence mapped
-	#if($gapRecord->[2] == $sourceProfile{$gapRecord->[0]}->{length} or 
-	#    $gapRecord->[1] == 0) {
-	#    return(&extendGap({gapRecord=>$gapRecord,mapping=>$mapping,fai=>$fai,id=>$id}));
-	#} else {
-	#    return;
-	#}
 	return(&extendGap({gapRecord=>$gapRecord,mapping=>$mapping,fai=>$fai,id=>$id}));
     }
+}
+sub twoAnchoredGap {
+    my ($anchor1mapping,$anchor2mapping) = &splitMappingbyAnchor($mapping);
+    #iterate through all possible pairs to look for concordant pairs
+    for my $i(@$anchor1mapping) {
+	for my $j(@$anchor2mapping) {
+    check if two flanking seq are concordant
+    check if distance between two flanking seq are within gaplen+tolerance
+    check orientation
+
+	}
+    }
+}
+sub splitMappingbyAnchor {
+    my $mapping = shift;
+    my %result;
+    for my $i(@$mapping) {
+	my $qid = $i->{qid};
+	if($result{$qid}) {
+	    push @{$result{$qid}},$i;
+	} else {
+	    $result{$qid} = [$i];
+	}
+    }
+    my $nFound = int(keys %result);
+    die "Two anchors expected, found $nFound\n" if $nFound != 2;
+    return(values %result);
 }
 sub extendGap {
     my $opt = shift;
@@ -129,8 +151,10 @@ sub extendGap {
 	$name = $id;
 	$score = $i->{e};
 	($start,$end,$strand,$status) = &decideGapLocus($targetProfile{$i->{sid}},$i,$gapRecord,$gapLen);
-	push @oneresult,$ctg, $start, $end, $name,$score, $strand, $status;
-	push @results,\@oneresult;
+	if(defined $start && defined $end) {
+	    push @oneresult,$ctg, $start, $end, $name,$score, $strand, $status;
+	    push @results,\@oneresult;
+	}
     }
     return(@results);
 }
@@ -141,10 +165,6 @@ sub decideGapLocus {
     my $gapLen = shift;
     my ($start,$end,$strand,$status);
     &fixMapping($mapping);
-    sub fixMapping {
-	my $mapping = shift;
-	if($mapping->{
-    }
     if($mapping->{strand} eq 'plus') {
 	#* for N, |----> for mapping
 	#case1
@@ -154,6 +174,7 @@ sub decideGapLocus {
 	$strand = "+";
 	if(&isUpstream($mapping,$gapRecord)) {
 	    #case2
+	    return if $mapping->{send} > $mapping->{slen};
 	    $start = $mapping->{send}; #-1 is not necessary, this is end for flanking seq
 	    if($ctgInfo->{length} >= $mapping->{send} + $gapLen) {
 		#gap can be fully filled
@@ -166,6 +187,7 @@ sub decideGapLocus {
 	    }
 	} else {
 	    #case1
+	    return if $mapping->{sstart} < 0;
 	    $end = $mapping->{sstart}-1; #-1 is necessary
 	    if($end - $gapLen >= 0) {
 		#gap can be fully filled
@@ -177,6 +199,10 @@ sub decideGapLocus {
 		$start = 0;
 	    }
 	}
+	if($start > $end) {
+	    &dumpMapping($mapping);
+	    die "start > end for + strand!\n";
+	}
     } elsif ($mapping->{strand} eq 'minus') {
 	#case3
 	#---*******<---|-------------------------
@@ -184,7 +210,8 @@ sub decideGapLocus {
 	#----------------------<---|**********---
 	$strand = "-";
 	if(&isUpstream($mapping,$gapRecord)) {
-	    #case2
+	    #case4
+	    return if $mapping->{sstart} > $mapping->{slen}; #out of bound
 	    $end = $mapping->{sstart}; #-1 is not necessary, this is end for flanking seq
 	    if($ctgInfo->{length} >= $end + $gapLen) {
 		#gap can be fully filled
@@ -196,7 +223,8 @@ sub decideGapLocus {
 		$start = $ctgInfo->{length};
 	    }
 	} else {
-	    #case1
+	    #case3
+	    return if $mapping->{send} < 0;# out of bound
 	    $start = $mapping->{send}-1; #-1 is necessary
 	    if($start - $gapLen >= 0) {
 		#gap can be fully filled
@@ -208,16 +236,42 @@ sub decideGapLocus {
 		$end = 0;
 	    }
 	}
+	if($start < $end) {
+	    &dumpMapping($mapping);
+	    die "start < end for - strand!\n";
+	}
     } else {
 	die "unknown strand:".$mapping->{strand}."\n";
     }
     if($start == $end) {
-	for my $i(keys %{$mapping}) {
-	    print "key:$i,value: ",$mapping->{$i},"\n";
-	}
+	&dumpMapping($mapping);
 	die "equal start and end !\n";
     }
     return($start,$end,$strand,$status);
+}
+sub dumpMapping {
+    my $mapping = shift;
+    for my $i(keys %{$mapping}) {
+	print "key:$i,value: ",$mapping->{$i},"\n";
+    }
+}
+sub fixMapping {
+    my $mapping = shift;
+    if(abs($mapping->{qend} - $mapping->{qstart}) + 1 == $mapping->{qlen}) {
+	#full length mapped, nothing to do
+	return;
+    } else {
+	#qstart<qend anytime
+	#sstart>send for - strand
+	#sstart<send for + strand
+	if($mapping->{strand} eq 'plus') {
+	    $mapping->{sstart} = $mapping->{sstart} - ($mapping->{qstart}-1);
+	    $mapping->{send}   = $mapping->{send} + ($mapping->{qlen} - $mapping->{qend});
+	} else {
+	    $mapping->{sstart} = $mapping->{sstart} + ($mapping->{qstart}-0);
+	    $mapping->{send}   = $mapping->{send} - ($mapping->{qlen} - $mapping->{qend} - 1);
+	}
+    }
 }
 sub isUpstream {
     my $mapping = shift;
@@ -252,17 +306,21 @@ sub filterMapping {
     my $mapping = shift;
     my @result;
 
+    warn "before filtering ",int(@$mapping),"\n" if $debug;
     for my $i(@$mapping) {
+	my $pass = 1;
 	if($filter->{'idt'}) {
-	    push @result,$i if $i->{idt} >= $filter->{idt};
+	    $pass = 0 if $i->{idt} < $filter->{idt};
 	}
 	if($filter->{'qcov'}) {
-	    push @result,$i if $i->{qcov} >= $filter->{qcov};
+	    $pass = 0 if $i->{qcov} < $filter->{qcov};
 	}
 	if($filter->{'e'}) {
-	    push @result,$i if $i->{e} <= $filter->{e};
+	    $pass = 0 if $i->{e} > $filter->{e};
 	}
+	push @result,$i if $pass;
     }
+    warn "after filtering ",int(@result),"\n" if $debug;
     return(\@result);
 }
 sub parseMapping {
@@ -272,20 +330,21 @@ sub parseMapping {
     open IN,'<',$f or die "open($f): $!\n";
     while(<IN>) {
 	my @f = split /,/;
-	die "expect 11 fields at line $. of $f\n" unless @f==11;
+	die "expect 12 fields at line $. of $f\n" unless @f==12;
 	#s is for subject, the reference seq
 	push @results,{
 	    qid	=>	$f[0],
 	    qlen	=>	$f[1],
 	    sid	=>	$f[2],
-	    qstart	=>	$f[3],
-	    qend	=>	$f[4],
-	    sstart	=>	$f[5],
-	    send	=>	$f[6],
-	    strand	=>	$f[7],
-	    e		=>	$f[8],
-	    idt	=>	$f[9],
-	    qcov	=>	$f[10],
+	    slen	=>	$f[3],
+	    qstart	=>	$f[4],
+	    qend	=>	$f[5],
+	    sstart	=>	$f[6],
+	    send	=>	$f[7],
+	    strand	=>	$f[8],
+	    e		=>	$f[9],
+	    idt	=>	$f[10],
+	    qcov	=>	$f[11],
 	};
     }
     close IN;
